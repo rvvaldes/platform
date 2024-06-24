941168013a50e34d7eff2994d25441df9223b8a8
Rodney Velazquez <rvelazquezv@deloitte.com>
2024-06-24 17:31:19 -0400

    test pipeline

--- code/front/src/vite-env.d.ts
+++ code/front/src/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
 VITE_JSON_SERVER_URL=https://jsonplaceholder.typicode.com
-VITE_URL_LOGIN=http://localhost:80
+VITE_URL_LOGIN=http://app.platform.svc.cluster.local:80

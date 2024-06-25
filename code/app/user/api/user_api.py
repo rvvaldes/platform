af3f0f9a3596d1a01ddb06b11b40052437e80d48
panchoven <pancho.asdf@gmail.com>
2024-06-25 16:40:32 -0400

    Se protege ruta users

--- code/app/user/api/user_api.py
+++ code/app/user/api/user_api.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
 from user.services.user_service import create_user, get_users, update_user, delete_user
 
 user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/users', methods=['POST'])
+@jwt_required()
 def api_create_user():
     data = request.json
     user = create_user(data)
     return jsonify(user.id), 201
 
 @user_api.route('/users', methods=['GET'])
+@jwt_required()
 def api_get_users():
     users = get_users()
     return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
 
 @user_api.route('/users/<int:id>', methods=['PUT'])
+@jwt_required()
 def api_update_user(id):
     data = request.json
     user = update_user(id, data)
@@ -23,6 +27,7 @@ def api_update_user(id):
     return 'User not found', 404
 
 @user_api.route('/users/<int:id>', methods=['DELETE'])
+@jwt_required()
 def api_delete_user(id):
     if delete_user(id):
         return 'User deleted', 200
